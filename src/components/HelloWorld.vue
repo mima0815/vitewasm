8dc5ec62d6c04cbb2820875cebb27013efdc62a1
Michael Matzka <14311597+mima0815@users.noreply.github.com>
2023-05-30 08:42:29 +0200

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import {detect as detectJson30, parse as parseJson30} from '@swagger-api/apidom-parser-adapter-openapi-json-3-0';
+
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+
+if (await detectJson30('')) {
+    // issueStore.parseResult = await parseJson30(editorStore.editorContent);
+  } 
+
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <button type="button" @click="count++">count is {{ count }}</button>
+    <p>
+      Edit
+      <code>components/HelloWorld.vue</code> to test HMR
+    </p>
+  </div>
+
+  <p>
+    Check out
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
+      >create-vue</a
+    >, the official Vue + Vite starter
+  </p>
+  <p>
+    Install
+    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
+    in your IDE for a better DX
+  </p>
+  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import wasm from 'vite-plugin-wasm';
+import topLevelAwait from 'vite-plugin-top-level-await';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(),    wasm(),
+    topLevelAwait(),],
+    worker: {
+      // Not needed with vite-plugin-top-level-await >= 1.3.0
+      // format: "es",
+      plugins: [
+        wasm(),
+        topLevelAwait()
+      ]
+    }
+})
